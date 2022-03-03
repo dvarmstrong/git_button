405f26e27da870f4e0ccabe7ac3e7acee0d867b4
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-03-02 23:44:17 -0500

    Intial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+});
+
+function getGit() {
+    fetch("https://api.github.com/users/adion81")
+    .then(response => response.json() )
+    .then(coderData => { 
+        console.log(coderData);
+        let coderName = coderData['name']
+        let coderFollowers = " Has " +  coderData['followers'] + " followers "
+        let coderAvatar = coderData['avatar_url']
+        console.log(coderName)
+        let ptag = document.querySelector("#name")
+        let img = document.querySelector("#avatar")
+        ptag.innerHTML= coderName + coderFollowers
+        img.src = coderAvatar
+    })  
+    .catch(err => console.log(err) );
+
+}
+    
+    function search() {
+        let user_input = document.querySelector("#input")
+        let search_term = user_input.value
+        console.log(search_term)
+        fetch(`https://api.github.com/users/${search_term}`)
+        .then(response => response.json() )
+        .then(coderData => { 
+            console.log(coderData);
+            let coderName = coderData['name']
+            let coderFollowers = " Has " +  coderData['followers'] + " followers "
+            let coderAvatar = coderData['avatar_url']
+            console.log(coderName)
+            let ptag = document.querySelector("#name")
+            let img = document.querySelector("#avatar")
+            ptag.innerHTML= coderName + coderFollowers
+            img.src = coderAvatar
+        })  
+        .catch(err => console.log(err) )
+    
+
+}
+
+
+
+
+// Keys I need from api:
+// avatar_url
+// name
+// followers
+
+
+
+// async function getCoderData() {
+//     var response = await fetch("https://api.github.com/users/");
+//     var coderData = await response.json();
+//     return coderData;
+
+// }
+
+// document.getElementById('button')
+
